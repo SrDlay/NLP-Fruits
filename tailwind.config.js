b4052e4c8d6424ed7cf1c68659cd3de9175ed787
Adriel Itamar <adrielitamar@gmail.com>
2022-11-26 13:43:06 -0300

    interface do projeto

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {'poppins':['Poppins', 'sans-serif']},
+    extend: {},
+  },
+  plugins: [],
+}
